e872f4a50eb280e609e20aa2bf4f95e2e6980285
AMmetro <metro.trend.now@gmail.com>
2025-03-03 18:52:01 +0300

    test55

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class Application {
 
     public static void main(String[] args) {
-//test 4444
+//test 4444 555
         SpringApplication.run(Application.class, args);
     }
 
